32125d7c6d1423411fa42479360040537b56b098
Otmane El bourki <oel-bour@e2r7p3.1337.ma>
2021-11-23 09:54:38 +0100

    first push

---
+++ serve_model.sh
@@ -0,0 +1,8 @@
+echo "Start Serving Model"
+
+sudo docker run -d -p 8501:8501 --name=pets -v "/home/rhyme/Desktop/app/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
+sleep 1
+echo "Done Serving"
+
+echo "Start App"
+python3 app.py
