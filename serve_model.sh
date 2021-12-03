8d86907fd6814c3cfb9f57454239912c50f72359
Otmane El bourki <oel-bour@e2r7p3.1337.ma>
2021-12-03 11:33:15 +0100

    change path to model

--- serve_model.sh
+++ serve_model.sh
@@ -1,6 +1,6 @@
 echo "Start Serving Model"
 
-sudo docker run -d -p 8501:8501 --name=pets -v "/home/rhyme/Desktop/app/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
+sudo docker run -d -p 8501:8501 --name=pets -v "$(pwd)/app/pets/:/models/pets/1" -e MODEL_NAME=pets tensorflow/serving
 sleep 1
 echo "Done Serving"
 
